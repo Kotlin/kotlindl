1d30d1edf75b1521ef6730446f7930fe45b0110c
Alexey Zinoviev <zaleslaw.sin@gmail.com>
2020-04-27 16:01:49 +0300

    Added a few activation functions

--- src/main/kotlin/tf_api/keras/activations/Activations.kt
+++ src/main/kotlin/tf_api/keras/activations/Activations.kt
@@ -8,26 +8,35 @@ enum class Activations {
     Sigmoid,
     Tanh,
     Relu,
+    Relu6,
     Elu,
     Selu,
     Softmax,
-    LogSoftmax;
+    LogSoftmax,
+    Exponential,
+    SoftPlus,
+    SoftSign;
 
     companion object {
         fun <T : Number> convert(activationType: Activations): Activation<T> {
             return when (activationType) {
-                Sigmoid -> SigmoidActivation<T>()
-                Linear -> LinearActivation<T>()
-                Tanh -> TanhActivation<T>()
-                Relu -> ReluActivation<T>()
-                Elu -> EluActivation<T>()
-                Selu -> TODO()
-                Softmax -> SoftmaxActivation<T>()
-                LogSoftmax -> LogSoftmaxActivation<T>()
+                Sigmoid -> SigmoidActivation()
+                Linear -> LinearActivation()
+                Tanh -> TanhActivation()
+                Relu -> ReluActivation()
+                Relu6 -> Relu6Activation()
+                Elu -> EluActivation()
+                Selu -> SeluActivation()
+                Softmax -> SoftmaxActivation()
+                LogSoftmax -> LogSoftmaxActivation()
+                Exponential -> ExponentialActivation()
+                SoftPlus -> SoftPlusActivation()
+                SoftSign -> SoftSignActivation()
             }
         }
     }
 }
+
 
 class LinearActivation<T : Number>() : Activation<T> {
     override fun apply(tf: Ops, features: Operand<T>): Operand<T> {
@@ -47,6 +56,12 @@ class ReluActivation<T : Number>() : Activation<T> {
     }
 }
 
+class Relu6Activation<T : Number>() : Activation<T> {
+    override fun apply(tf: Ops, features: Operand<T>): Operand<T> {
+        return tf.nn.relu6(features)
+    }
+}
+
 class TanhActivation<T : Number>() : Activation<T> {
     override fun apply(tf: Ops, features: Operand<T>): Operand<T> {
         return tf.math.tanh(features)
@@ -59,6 +74,12 @@ class EluActivation<T : Number>() : Activation<T> {
     }
 }
 
+class SeluActivation<T : Number>() : Activation<T> {
+    override fun apply(tf: Ops, features: Operand<T>): Operand<T> {
+        return tf.nn.selu(features)
+    }
+}
+
 class SoftmaxActivation<T : Number>() : Activation<T> {
     override fun apply(tf: Ops, features: Operand<T>): Operand<T> {
         return tf.nn.softmax(features)
@@ -68,5 +89,23 @@ class SoftmaxActivation<T : Number>() : Activation<T> {
 class LogSoftmaxActivation<T : Number>() : Activation<T> {
     override fun apply(tf: Ops, features: Operand<T>): Operand<T> {
         return tf.nn.logSoftmax(features)
+    }
+}
+
+class ExponentialActivation<T : Number>() : Activation<T> {
+    override fun apply(tf: Ops, features: Operand<T>): Operand<T> {
+        return tf.math.exp(features)
+    }
+}
+
+class SoftPlusActivation<T : Number>() : Activation<T> {
+    override fun apply(tf: Ops, features: Operand<T>): Operand<T> {
+        return tf.math.log(tf.math.add(tf.math.exp(features), tf.constant(1) as Operand<T>))
+    }
+}
+
+class SoftSignActivation<T : Number>() : Activation<T> {
+    override fun apply(tf: Ops, features: Operand<T>): Operand<T> {
+        return tf.nn.softsign(features)
     }
 }
