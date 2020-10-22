e1a28d4dac274388a049bc31c77759990251f9a6
Alexey Zinoviev <zaleslaw.sin@gmail.com>
2020-10-22 11:53:12 +0300

    Added license

--- api/src/main/kotlin/api/core/layer/twodim/AvgPool2D.kt
+++ api/src/main/kotlin/api/core/layer/twodim/AvgPool2D.kt
@@ -1,5 +1,10 @@
 package api.core.layer.twodim
 
+/*
+ * Copyright 2020 JetBrains s.r.o. and Kotlin Deep Learning project contributors. All Rights Reserved.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
+
 import api.core.KGraph
 import api.core.layer.Layer
 import api.core.shape.convOutputLength
--- examples/src/main/java/LeNetClassic.java
+++ examples/src/main/java/LeNetClassic.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 JetBrains s.r.o. and Kotlin Deep Learning project contributors. All Rights Reserved.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
+
 import api.core.Sequential;
 import api.core.activation.Activations;
 import api.core.callback.Callback;
