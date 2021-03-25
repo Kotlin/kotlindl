7d774d477b33a14a6cb41a10ccd2223bc677fb53
Alexey Zinoviev <zaleslaw.sin@gmail.com>
2021-03-25 17:57:22 +0300

    Removed outdated code

--- api/src/main/kotlin/org/jetbrains/kotlinx/dl/datasets/Cropping.kt
+++ api/src/main/kotlin/org/jetbrains/kotlinx/dl/datasets/Cropping.kt
@@ -35,30 +35,4 @@ public class Cropping(
 
         return Pair(croppedImage, croppedImageShape)
     }
-    /*override fun apply(image: FloatArray, inputShape: ImageShape): Pair<FloatArray, ImageShape> {
-        // TODO: check input parameters with inputShape on logic
-        val croppedImageShape =
-            ImageShape(
-                width = inputShape.width - left - right,
-                height = inputShape.height - top - bottom,
-                channels = inputShape.channels
-            )
-        val croppedImage = FloatArray(croppedImageShape.numberOfElements.toInt())
-
-        for (i in 0 until croppedImageShape.width.toInt()) {
-            for (j in 0 until croppedImageShape.height.toInt()) {
-                for (k in 0 until croppedImageShape.channels.toInt()) {
-                    croppedImage.set3D(
-                        i, j, k, croppedImageShape.width.toInt(), croppedImageShape.channels.toInt(), image.get3D(
-                            i + top, j + left, k,
-                            inputShape.width.toInt(),
-                            inputShape.channels.toInt()
-                        )
-                    )
-                }
-            }
-        }
-
-        return Pair(croppedImage, croppedImageShape)
-    }*/
 }
