f0120e14cd80d71aad244fd313026cf1737427a4
D. Lowl <d.lowl@d-lowl.space>
2021-05-27 22:46:21 +0500

    Add Softmax layer docstring

--- api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/layer/activation/Softmax.kt
+++ api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/layer/activation/Softmax.kt
@@ -3,6 +3,22 @@ package org.jetbrains.kotlinx.dl.api.core.layer.activation
 import org.tensorflow.Operand
 import org.tensorflow.op.Ops
 
+/**
+ * Softmax activation layer
+ *
+ * Rescales input such that the elements in [axis] sum up to 1.
+ *
+ * For each batch `i` and class `j` we have
+ *
+ * ```
+ * softmax[i, j] = exp(logits[i, j]) / sum_j(exp(logits[i, j]))
+ * ```
+ *
+ * @property [axis] Axes to apply softmax to (NOTE: the property exist for config compatibility,
+ * only the last axis is supported)
+ * @constructor Creates [Softmax] object
+ * @since 0.3
+ */
 public class Softmax(
     public val axis: List<Int> = listOf(-1),
     name: String = ""
