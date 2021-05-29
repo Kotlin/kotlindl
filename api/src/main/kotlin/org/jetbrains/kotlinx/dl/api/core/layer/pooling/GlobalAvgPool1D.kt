93b47febaf358f849e4dde0afb79d921b8a61432
therealansh <tyagiansh23@hotmail.com>
2021-05-30 03:36:10 +0530

    add: docs to GlobalAvgPool1D

--- api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/layer/pooling/GlobalAvgPool1D.kt
+++ api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/layer/pooling/GlobalAvgPool1D.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 JetBrains s.r.o. and Kotlin Deep Learning project contributors. All Rights Reserved.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
+
 package org.jetbrains.kotlinx.dl.api.core.layer.pooling
 
 import org.jetbrains.kotlinx.dl.api.core.KGraph
@@ -6,6 +11,20 @@ import org.jetbrains.kotlinx.dl.api.core.util.TF
 import org.tensorflow.Operand
 import org.tensorflow.Shape
 import org.tensorflow.op.Ops
+
+/**
+ * Global average pooling operation for temporal data.
+ *
+ * NOTE: Works with tensors which must have rank 3 (batch, steps, features).
+ *
+ * Input shape: 3D tensor with shape `(batch_size, steps, features)`.
+ *
+ * Output shape: 2D tensor with shape `(batch_size, features)`.
+ *
+ * @property [name] Custom layer name.
+ * @constructor Creates [GlobalAvgPool1D] object.
+ *
+ */
 
 class GlobalAvgPool1D(
         name: String = ""
@@ -17,8 +36,8 @@ class GlobalAvgPool1D(
     }
 
     override fun forward(tf: Ops, input: Operand<Float>, isTraining: Operand<Boolean>, numberOfLosses: Operand<Float>?): Operand<Float> {
-        var stepAxis = 1                //if dataFormat == "channels_last" will be 2 if "channels_first"
-        return TF.mean(tf, input, tf.constant(stepAxis))
+        var stepAxis = 1                //if dataFormat == "channels_last", it will be 2 if "channels_first"
+        return TF.mean(tf, input, tf.constant(stepAxis))        //mask == None
     }
 
     override val weights: Map<String, Array<*>> get() = emptyMap()
@@ -28,6 +47,6 @@ class GlobalAvgPool1D(
     override val paramCount: Int get() = 0
 
     override fun toString(): String {
-        return "GlobalAvgPool1D()"
+        return "GlobalAvgPool1D(name=$name)"
     }
 }
