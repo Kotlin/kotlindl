459e233e04c5893f516e69c1037746c5c28e4ba6
therealansh <tyagiansh23@hotmail.com>
2021-05-31 18:22:36 +0530

    add: unit test for GlobalAvgPooling1D

---
+++ api/src/test/kotlin/org/jetbrains/kotlinx/dl/api/core/layer/GlobalAvgPooling1DTest.kt
@@ -0,0 +1,14 @@
+package org.jetbrains.kotlinx.dl.api.core.layer
+
+import org.jetbrains.kotlinx.dl.api.core.layer.pooling.GlobalAvgPool1D
+import org.junit.jupiter.api.Test
+
+internal class GlobalAvgPooling1DTest : PoolLayerTest() {
+
+    @Test
+    fun globalAvgPool1DTest(){
+        val input = Array(2, { Array(3, { FloatArray(4) { 0.toFloat() } } ) } )
+        val expected = Array(2, {FloatArray(4) { 0.toFloat() } })
+        assertGlobalAvgPool1DEquals(GlobalAvgPool1D(),input, expected )
+    }
+}
