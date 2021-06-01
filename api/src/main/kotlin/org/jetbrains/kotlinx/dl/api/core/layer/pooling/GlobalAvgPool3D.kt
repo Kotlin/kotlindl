86a596548ab5d7e067e11d9d6a9f5a16f78dc19f
therealansh <tyagiansh23@hotmail.com>
2021-06-02 01:21:08 +0530

    add: docs for GlobalAvgPool3D Layer

--- api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/layer/pooling/GlobalAvgPool3D.kt
+++ api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/layer/pooling/GlobalAvgPool3D.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 JetBrains s.r.o. and Kotlin Deep Learning project contributors. All Rights Reserved.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
+
 package org.jetbrains.kotlinx.dl.api.core.layer.pooling
 
 import org.jetbrains.kotlinx.dl.api.core.KGraph
@@ -7,6 +12,14 @@ import org.tensorflow.Operand
 import org.tensorflow.Shape
 import org.tensorflow.op.Ops
 
+/**
+ * Global Average pooling operation for 3D data.
+ * NOTE: Works with tensors which must have rank 5 (batch_size, spatial_dim1, spatial_dim2, spatial_dim3, channels).
+ * Input shape: 5D tensor with shape `(batch_size, spatial_dim1, spatial_dim2, spatial_dim3, channels)`.
+ * Output shape: 2D tensor with shape `(batch_size, channels)`.
+ * @property [name] Custom layer name.
+ * @constructor Creates [GlobalAvgPool3D] object.
+ */
 public class GlobalAvgPool3D(
     name: String = ""
 ):Layer(name) {
@@ -27,16 +40,13 @@ public class GlobalAvgPool3D(
         return TF.mean(tf, input, tf.constant(intArrayOf(1, 2, 3)))
     }
 
-    override val weights: Map<String, Array<*>>
-        get() = emptyMap()
+    override val weights: Map<String, Array<*>> get() = emptyMap()
 
-    override val hasActivation: Boolean
-        get() = false
+    override val hasActivation: Boolean get() = false
 
-    override val paramCount: Int
-        get() = 0
+    override val paramCount: Int get() = 0
 
     override fun toString(): String {
-        return "GlobalAvgPool3D()"
+        return "GlobalAvgPool3D(name=$name)"
     }
 }
