0bd18f537b96d379156ef5e2690542a80c0a5366
Alexey Zinoviev <zaleslaw.sin@gmail.com>
2021-06-03 13:24:01 +0300

    Fixed Java example

--- examples/src/main/java/LeNetClassic.java
+++ examples/src/main/java/LeNetClassic.java
@@ -5,13 +5,26 @@
 
 import kotlin.Pair;
 import org.jetbrains.kotlinx.dl.api.core.Sequential;
+import org.jetbrains.kotlinx.dl.api.core.activation.Activations;
 import org.jetbrains.kotlinx.dl.api.core.callback.Callback;
+import org.jetbrains.kotlinx.dl.api.core.initializer.Constant;
+import org.jetbrains.kotlinx.dl.api.core.initializer.GlorotNormal;
+import org.jetbrains.kotlinx.dl.api.core.initializer.Zeros;
+import org.jetbrains.kotlinx.dl.api.core.layer.convolutional.Conv2D;
+import org.jetbrains.kotlinx.dl.api.core.layer.convolutional.ConvPadding;
+import org.jetbrains.kotlinx.dl.api.core.layer.core.Dense;
+import org.jetbrains.kotlinx.dl.api.core.layer.core.Input;
+import org.jetbrains.kotlinx.dl.api.core.layer.pooling.MaxPool2D;
+import org.jetbrains.kotlinx.dl.api.core.layer.reshaping.Flatten;
 import org.jetbrains.kotlinx.dl.api.core.loss.SoftmaxCrossEntropyWithLogits;
 import org.jetbrains.kotlinx.dl.api.core.metric.Metrics;
 import org.jetbrains.kotlinx.dl.api.core.optimizer.Adam;
 import org.jetbrains.kotlinx.dl.api.core.optimizer.NoClipGradient;
+import org.jetbrains.kotlinx.dl.api.core.regularizer.L1;
+import org.jetbrains.kotlinx.dl.api.core.regularizer.L2;
 import org.jetbrains.kotlinx.dl.dataset.EmbeddedDatasetsKt;
 import org.jetbrains.kotlinx.dl.dataset.OnHeapDataset;
+import org.jetbrains.kotlinx.dl.dataset.handler.MnistUtilKt;
 
 import java.io.File;
 
@@ -33,15 +46,15 @@ public class LeNetClassic {
         OnHeapDataset test = result.component2();
 
         try (Sequential lenet5Classic = Sequential.of(
-                /*new Input(new long[]{IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS}, "x"),
-                new Conv2D(6, new long[]{5, 5}, new long[]{1, 1, 1, 1}, new long[]{1, 1, 1, 1}, Activations.Tanh, new GlorotNormal(SEED), new Zeros(), ConvPadding.SAME, true, "conv2d_1"),
+                new Input(new long[]{IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS}, "x"),
+                new Conv2D(6, new long[]{5, 5}, new long[]{1, 1, 1, 1}, new long[]{1, 1, 1, 1}, Activations.Tanh, new GlorotNormal(SEED), new Zeros(), null, null, null, ConvPadding.SAME, true, "conv2d_1"),
                 new MaxPool2D(new int[]{1, 2, 2, 1}, new int[]{1, 2, 2, 1}, ConvPadding.VALID, "maxPool_1"),
-                new Conv2D(16, new long[]{5, 5}, new long[]{1, 1, 1, 1}, new long[]{1, 1, 1, 1}, Activations.Tanh, new GlorotNormal(SEED), new Zeros(), ConvPadding.SAME, true, "conv2d_2"),
+                new Conv2D(16, new long[]{5, 5}, new long[]{1, 1, 1, 1}, new long[]{1, 1, 1, 1}, Activations.Tanh, new GlorotNormal(SEED), new Zeros(), null, null, null, ConvPadding.SAME, true, "conv2d_2"),
                 new MaxPool2D(new int[]{1, 2, 2, 1}, new int[]{1, 2, 2, 1}, ConvPadding.VALID, "maxPool_2"),
                 new Flatten(), // 3136
-                new Dense(120, Activations.Tanh, new GlorotNormal(SEED), new Constant(0.1f), true, "dense_1"),
-                new Dense(84, Activations.Tanh, new GlorotNormal(SEED), new Constant(0.1f), true, "dense_2"),
-                new Dense(MnistUtilKt.NUMBER_OF_CLASSES, Activations.Linear, new GlorotNormal(SEED), new Constant(0.1f), true, "dense_3")*/
+                new Dense(120, Activations.Tanh, new GlorotNormal(SEED), new Constant(0.1f), null, null, null, true, "dense_1"),
+                new Dense(84, Activations.Tanh, new GlorotNormal(SEED), new Constant(0.1f), null, null, null, true, "dense_2"),
+                new Dense(MnistUtilKt.NUMBER_OF_CLASSES, Activations.Linear, new GlorotNormal(SEED), new Constant(0.1f), new L2(0.001f), new L1(0.0001f), null, true, "dense_3")
         )) {
 
             Adam adam = new Adam(0.001f, 0.9f, 0.999f, 1e-07f, false, new NoClipGradient());
