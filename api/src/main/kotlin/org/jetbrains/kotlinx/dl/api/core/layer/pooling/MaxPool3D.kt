2b590f455b8733ff0369d1d3790f0aa877949773
therealansh <tyagiansh23@hotmail.com>
2021-06-05 13:02:22 +0530

    add:docs to MaxPool3D

--- api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/layer/pooling/MaxPool3D.kt
+++ api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/layer/pooling/MaxPool3D.kt
@@ -4,6 +4,7 @@ import org.jetbrains.kotlinx.dl.api.core.KGraph
 import org.jetbrains.kotlinx.dl.api.core.layer.Layer
 import org.jetbrains.kotlinx.dl.api.core.layer.convolutional.ConvPadding
 import org.jetbrains.kotlinx.dl.api.core.shape.convOutputLength
+import org.jetbrains.kotlinx.dl.api.core.shape.shapeOperand
 import org.jetbrains.kotlinx.dl.api.inference.keras.CHANNELS_FIRST
 import org.jetbrains.kotlinx.dl.api.inference.keras.CHANNELS_LAST
 import org.tensorflow.Operand
@@ -11,6 +12,17 @@ import org.tensorflow.Shape
 import org.tensorflow.op.Ops
 import java.util.*
 
+/**
+ * Max pooling operation for 3D data (spatial or spatio-temporal).
+ * NOTE: Works with tensors which must have rank 5 (batch, depth, height, width, channels).
+ * @property [dataFormat] The ordering of the dimensions in the inputs. (CHANNEL_FIRST, CHANNEL_LAST)
+ * @property [poolSize] The size of the sliding window for each dimension of input tensor (pool batch, pool depth ,pool height, pool width, pool channels).
+ * Usually, pool batch and pool channels are equal to 1.
+ * @property [strides] Strides of the pooling operation for each dimension of input tensor.
+ * @property [padding] The padding method, either 'valid' or 'same'.
+ * @property [name] Custom layer name.
+ * @constructor Creates [MaxPool2D] object.
+ */
 class MaxPool3D(
     public val dataFormat: String = CHANNELS_LAST,
     public var poolSize: IntArray = intArrayOf(1, 2, 2, 2, 1),
@@ -53,8 +65,16 @@ class MaxPool3D(
         val paddingName = padding.paddingName
         var tfPoolSize = Arrays.stream(poolSize).asLongStream().toArray();
         var tfStrides = Arrays.stream(strides ?: poolSize).asLongStream().toArray();
-//        if(dataFormat== CHANNELS_FIRST) input = transpose()
-        return tf.nn.maxPool3d(input,tfPoolSize.toList(), tfStrides.toList(), paddingName,  )
+        var tfInput:Operand<Float> = input
+        if(dataFormat== CHANNELS_FIRST) {
+            tfInput = tf.linalg.transpose(input, shapeOperand(tf, Shape.make(0, 2, 3, 4, 1)))
+        }
+
+        var output = tf.nn.maxPool3d(tfInput,tfPoolSize.toList(), tfStrides.toList(), paddingName,  )
+        if(dataFormat== CHANNELS_FIRST){
+            return tf.linalg.transpose(output, shapeOperand(tf, Shape.make(0,4,1,2,3)))
+        }
+        return output
     }
 
     override val weights: Map<String, Array<*>> get() = emptyMap()
