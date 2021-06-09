e8d78b9611e54de9f128f8934347db22a9a56f22
Ansh Tyagi <tyagiansh23@hotmail.com>
2021-06-09 19:33:02 +0530

    Add GlobalAveragePool3D Layer (#91)
    * impl: Add GlobalAvgPool3D Layer
    
    * add: docs for GlobalAvgPool3D Layer
    
    * add: unit tests for GlobalAvgPool3d and GlobalAvgPool2d
    
    * add: dataFormat support in GlobalAvgPool3d
    
    * resolve: removed dataFormat
    
    * chore: formatting
    
    * chore: weights prop

--- api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/inference/keras/KerasConstants.kt
+++ api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/inference/keras/KerasConstants.kt
@@ -38,6 +38,7 @@ internal const val LAYER_MINIMUM: String = "Minimum"
 internal const val LAYER_CONCATENATE: String = "Concatenate"
 internal const val LAYER_GLOBAL_AVG_POOLING_2D: String = "GlobalAveragePooling2D"
 internal const val LAYER_GLOBAL_AVG_POOLING_1D: String = "GlobalAveragePooling1D"
+internal const val LAYER_GLOBAL_AVG_POOLING_3D: String = "GlobalAveragePooling3D"
 internal const val LAYER_SOFTMAX: String = "Softmax"
 
 // Keras data types
