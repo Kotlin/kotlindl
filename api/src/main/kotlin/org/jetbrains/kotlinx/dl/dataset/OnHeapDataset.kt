5df0b069b8ca40bdbdb8d89e9477f1e96d5696cc
avan1235 <macieekprocyk@gmail.com>
2021-06-13 18:02:48 +0200

    refactor to use sequences in stead of creating arrays

--- api/src/main/kotlin/org/jetbrains/kotlinx/dl/dataset/OnHeapDataset.kt
+++ api/src/main/kotlin/org/jetbrains/kotlinx/dl/dataset/OnHeapDataset.kt
@@ -293,7 +293,7 @@ public class OnHeapDataset internal constructor(private val x: Array<FloatArray>
  * @param lowPercent percent of data of [maxSize] to be printed from the beginning of array data.
  * Rest will be obtained from the tail of the array in order matching the order in array
  * @return string representation of [FloatArray] in format like
- * `[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, ..., 18.0, 19.0]`
+ * `[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, ..., 9.0, 10.0]`
  */
 private fun FloatArray.partialToString(maxSize: Int = 10, lowPercent: Double = 0.8): String {
     if (size <= maxSize) {
@@ -303,13 +303,13 @@ private fun FloatArray.partialToString(maxSize: Int = 10, lowPercent: Double = 0
     val lowCount = (lowPercent * maxSize).roundToInt()
     val upStart = size - maxSize - 1
 
-    return Array(maxSize + 1) {
+    return generateSequence(0, Int::inc).map {
         when {
             it < lowCount -> this[it]
             it > lowCount -> this[upStart + it]
             else -> "..."
         }
-    }.joinToString(prefix = "[", postfix = "]", separator = ", ")
+    }.take(maxSize + 1).joinToString(prefix = "[", postfix = "]", separator = ", ")
 }
 
 /**
@@ -319,17 +319,17 @@ private fun FloatArray.partialToString(maxSize: Int = 10, lowPercent: Double = 0
  * @param lowPercent percent of data of [maxSize] to be printed from the beginning of array data.
  * Rest will be obtained from the tail of the array in order matching the order in array
  * @return string representation of [FloatArray] in format like
- * `[[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, ..., 48.0, 49.0],
- *   [100.0, 101.0, 102.0, 103.0, 104.0, 105.0, 106.0, 107.0, ..., 148.0, 149.0],
- *   [200.0, 201.0, 202.0, 203.0, 204.0, 205.0, 206.0, 207.0, ..., 248.0, 249.0],
- *   [300.0, 301.0, 302.0, 303.0, 304.0, 305.0, 306.0, 307.0, ..., 348.0, 349.0],
- *   [400.0, 401.0, 402.0, 403.0, 404.0, 405.0, 406.0, 407.0, ..., 448.0, 449.0],
- *   [500.0, 501.0, 502.0, 503.0, 504.0, 505.0, 506.0, 507.0, ..., 548.0, 549.0],
- *   [600.0, 601.0, 602.0, 603.0, 604.0, 605.0, 606.0, 607.0, ..., 648.0, 649.0],
- *   [700.0, 701.0, 702.0, 703.0, 704.0, 705.0, 706.0, 707.0, ..., 748.0, 749.0],
+ * `[[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, ..., 9.0, 10.0],
+ *   [11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, ..., 20.0, 21.0],
+ *   [22.0, 23.0, 24.0, 25.0, 26.0, 27.0, 28.0, 29.0, ..., 31.0, 32.0],
+ *   [33.0, 34.0, 35.0, 36.0, 37.0, 38.0, 39.0, 40.0, ..., 42.0, 43.0],
+ *   [44.0, 45.0, 46.0, 47.0, 48.0, 49.0, 50.0, 51.0, ..., 53.0, 54.0],
+ *   [55.0, 56.0, 57.0, 58.0, 59.0, 60.0, 61.0, 62.0, ..., 64.0, 65.0],
+ *   [66.0, 67.0, 68.0, 69.0, 70.0, 71.0, 72.0, 73.0, ..., 75.0, 76.0],
+ *   [77.0, 78.0, 79.0, 80.0, 81.0, 82.0, 83.0, 84.0, ..., 86.0, 87.0],
  *   ...,
- *   [4800.0, 4801.0, 4802.0, 4803.0, 4804.0, 4805.0, 4806.0, 4807.0, ..., 4848.0, 4849.0],
- *   [4900.0, 4901.0, 4902.0, 4903.0, 4904.0, 4905.0, 4906.0, 4907.0, ..., 4948.0, 4949.0]]`
+ *   [99.0, 100.0, 101.0, 102.0, 103.0, 104.0, 105.0, 106.0, ..., 108.0, 109.0],
+ *   [110.0, 111.0, 112.0, 113.0, 114.0, 115.0, 116.0, 117.0, ..., 119.0, 120.0]]`
  */
 private fun Array<FloatArray>.partialToString(maxSize: Int = 10, lowPercent: Double = 0.8): String {
     if (size <= maxSize) {
@@ -341,11 +341,11 @@ private fun Array<FloatArray>.partialToString(maxSize: Int = 10, lowPercent: Dou
     val lowCount = (lowPercent * maxSize).roundToInt()
     val upStart = size - maxSize - 1
 
-    return Array(maxSize + 1) {
+    return generateSequence(0, Int::inc).map {
         when {
             it < lowCount -> this[it].partialToString(maxSize, lowPercent)
             it > lowCount -> this[upStart + it].partialToString(maxSize, lowPercent)
             else -> "..."
         }
-    }.joinToString(prefix = "[", postfix = "]", separator = ",\n ")
+    }.take(maxSize + 1).joinToString(prefix = "[", postfix = "]", separator = ",\n ")
 }
