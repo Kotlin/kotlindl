81f871ff58048b065deb5eff486ab1ef0baf26f2
therealansh <tyagiansh23@hotmail.com>
2021-06-24 23:44:46 +0530

    add: unit tests for Permute Layer

--- api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/layer/reshaping/Permute.kt
+++ api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/layer/reshaping/Permute.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2021 JetBrains s.r.o. and Kotlin Deep Learning project contributors. All Rights Reserved.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
 package org.jetbrains.kotlinx.dl.api.core.layer.reshaping
 
 import org.jetbrains.kotlinx.dl.api.core.KGraph
@@ -58,6 +62,6 @@ public class Permute(
     override val paramCount: Int get() = 0
 
     override fun toString(): String {
-        return "Permute(name=$name)"
+        return "Permute(name=$name, dims=$dims)"
     }
 }
