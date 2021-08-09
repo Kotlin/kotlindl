ec65e9ef874e5e02c96ce85a68f4b64b93039f6f
Masoud Kazemi <mkazemi73@gmail.com>
2021-08-09 19:12:59 +0430

    Add TimeStopping callback

---
+++ examples/src/main/kotlin/examples/cnn/mnist/advanced/LeNetWithTimeStoppingCallback.kt
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2021 JetBrains s.r.o. and Kotlin Deep Learning project contributors. All Rights Reserved.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
+
+package examples.cnn.mnist.advanced
+
+import org.jetbrains.kotlinx.dl.api.core.Sequential
+import org.jetbrains.kotlinx.dl.api.core.activation.Activations
+import org.jetbrains.kotlinx.dl.api.core.callback.TimeStopping
+import org.jetbrains.kotlinx.dl.api.core.initializer.Constant
+import org.jetbrains.kotlinx.dl.api.core.initializer.GlorotNormal
+import org.jetbrains.kotlinx.dl.api.core.initializer.Zeros
+import org.jetbrains.kotlinx.dl.api.core.layer.convolutional.Conv2D
+import org.jetbrains.kotlinx.dl.api.core.layer.convolutional.ConvPadding
+import org.jetbrains.kotlinx.dl.api.core.layer.core.Dense
+import org.jetbrains.kotlinx.dl.api.core.layer.core.Input
+import org.jetbrains.kotlinx.dl.api.core.layer.pooling.AvgPool2D
+import org.jetbrains.kotlinx.dl.api.core.layer.reshaping.Flatten
+import org.jetbrains.kotlinx.dl.api.core.loss.Losses
+import org.jetbrains.kotlinx.dl.api.core.metric.Metrics
+import org.jetbrains.kotlinx.dl.api.core.optimizer.Adam
+import org.jetbrains.kotlinx.dl.api.core.optimizer.ClipGradientByValue
+import org.jetbrains.kotlinx.dl.dataset.handler.NUMBER_OF_CLASSES
+import org.jetbrains.kotlinx.dl.dataset.mnist
+
+private const val EPOCHS = 10
+private const val TRAINING_BATCH_SIZE = 1000
+private const val NUM_CHANNELS = 1L
+private const val IMAGE_SIZE = 28L
+private const val SEED = 12L
+private const val TEST_BATCH_SIZE = 1000
+
+/**
+ * This is an CNN based on an implementation of LeNet-5 from classic paper trained with TimeStopping callback.
+ *
+ * @see <a href="http://yann.lecun.com/exdb/publis/pdf/lecun-98.pdf">
+ *    Gradient-based learning applied to document recognition:[Yann LeCun, Léon Bottou, Yoshua Bengio, Patrick Haffner, 1998]</a>
+ */
+private val lenet5Classic = Sequential.of(
+    Input(
+        IMAGE_SIZE,
+        IMAGE_SIZE,
+        NUM_CHANNELS
+    ),
+    Conv2D(
+        filters = 6,
+        kernelSize = longArrayOf(5, 5),
+        strides = longArrayOf(1, 1, 1, 1),
+        activation = Activations.Tanh,
+        kernelInitializer = GlorotNormal(SEED),
+        biasInitializer = Zeros(),
+        padding = ConvPadding.SAME
+    ),
+    AvgPool2D(
+        poolSize = intArrayOf(1, 2, 2, 1),
+        strides = intArrayOf(1, 2, 2, 1),
+        padding = ConvPadding.VALID
+    ),
+    Conv2D(
+        filters = 16,
+        kernelSize = longArrayOf(5, 5),
+        strides = longArrayOf(1, 1, 1, 1),
+        activation = Activations.Tanh,
+        kernelInitializer = GlorotNormal(SEED),
+        biasInitializer = Zeros(),
+        padding = ConvPadding.SAME
+    ),
+    AvgPool2D(
+        poolSize = intArrayOf(1, 2, 2, 1),
+        strides = intArrayOf(1, 2, 2, 1),
+        padding = ConvPadding.VALID
+    ),
+    Flatten(), // 3136
+    Dense(
+        outputSize = 120,
+        activation = Activations.Tanh,
+        kernelInitializer = GlorotNormal(SEED),
+        biasInitializer = Constant(0.1f)
+    ),
+    Dense(
+        outputSize = 84,
+        activation = Activations.Tanh,
+        kernelInitializer = GlorotNormal(SEED),
+        biasInitializer = Constant(0.1f)
+    ),
+    Dense(
+        outputSize = NUMBER_OF_CLASSES,
+        activation = Activations.Linear,
+        kernelInitializer = GlorotNormal(SEED),
+        biasInitializer = Constant(0.1f)
+    )
+)
+
+/**
+ * This example shows how to do image classification from scratch using [lenet5Classic], without leveraging pre-trained weights or a pre-made model.
+ * We demonstrate the workflow on the Mnist classification dataset.
+ *
+ * It includes:
+ * - dataset loading from S3
+ * - callback definition
+ * - model compilation with [TimeStopping] callback
+ * - model summary
+ * - model training
+ * - model evaluation
+ */
+fun lenetWithTimeStoppingCallback() {
+    val (train, test) = mnist()
+
+    lenet5Classic.use {
+        val timeStopping = TimeStopping(seconds = 30, verbose = true)
+        it.compile(
+            optimizer = Adam(clipGradient = ClipGradientByValue(0.1f)),
+            loss = Losses.SOFT_MAX_CROSS_ENTROPY_WITH_LOGITS,
+            metric = Metrics.ACCURACY,
+            callback = timeStopping,
+        )
+
+        it.summary()
+
+        it.fit(dataset = train, epochs = EPOCHS, batchSize = TRAINING_BATCH_SIZE)
+
+        val accuracy = it.evaluate(dataset = test, batchSize = TEST_BATCH_SIZE).metrics[Metrics.ACCURACY]
+
+        println("Accuracy: $accuracy")
+    }
+}
+
+fun main(): Unit = lenetWithTimeStoppingCallback()
