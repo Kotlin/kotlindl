ae208e6e9d4a8a171c85dac219f7a7c322d5e698
Alexey Zinoviev <zaleslaw.sin@gmail.com>
2021-08-11 21:50:30 +0500

    Added COCO dataset labels

--- examples/src/main/kotlin/examples/onnx/yolo/prediction.kt
+++ examples/src/main/kotlin/examples/onnx/yolo/prediction.kt
@@ -10,14 +10,11 @@ import org.jetbrains.kotlinx.dl.api.core.Functional
 import org.jetbrains.kotlinx.dl.api.inference.keras.loaders.ModelType
 import org.jetbrains.kotlinx.dl.api.inference.keras.loaders.ModelZoo
 import org.jetbrains.kotlinx.dl.api.inference.onnx.OnnxInferenceModel
+import org.jetbrains.kotlinx.dl.dataset.handler.cocoCategories
 import org.jetbrains.kotlinx.dl.dataset.image.ColorOrder
-import org.jetbrains.kotlinx.dl.dataset.mnist
-import org.jetbrains.kotlinx.dl.dataset.preprocessor.ImageShape
-import org.jetbrains.kotlinx.dl.dataset.preprocessor.Preprocessing
+import org.jetbrains.kotlinx.dl.dataset.preprocessor.*
 import org.jetbrains.kotlinx.dl.dataset.preprocessor.image.load
 import org.jetbrains.kotlinx.dl.dataset.preprocessor.image.resize
-import org.jetbrains.kotlinx.dl.dataset.preprocessor.preprocess
-import org.jetbrains.kotlinx.dl.dataset.preprocessor.transformImage
 import java.io.File
 
 private const val PATH_TO_MODEL = "examples/src/main/resources/models/onnx/yolov4.onnx"
@@ -26,14 +23,12 @@ fun main() {
     val modelZoo = ModelZoo(commonModelDirectory = File("cache/pretrainedModels"), modelType = ModelType.MobileNet)
     val model = modelZoo.loadModel() as Functional
 
-    val imageNetClassLabels = modelZoo.loadClassLabels()
-
     OnnxInferenceModel.load(PATH_TO_MODEL).use {
         println(it)
 
         it.reshape(416, 416, 3)
 
-        for (i in 1..8) {
+        for (i in 0..8) {
             val preprocessing: Preprocessing = preprocess {
                 transformImage {
                     load {
@@ -46,27 +41,48 @@ fun main() {
                         outputWidth = 416
                     }
                 }
+                transformTensor {
+                    rescale {
+                        scalingCoefficient = 255.0f
+                    }
+                }
             }
 
             // TODO: currently, the whole model is loaded but not used for prediction, the preprocessing is used only
             // Correct preprocessing https://github.com/onnx/models/tree/master/vision/classification/efficientnet-lite4
             val inputData = modelZoo.preprocessInput(preprocessing().first, model.inputDimensions)
 
-            val res = it.rawPredict(inputData)
-            println(res)
-            // TODO: interpret the results of YOLOv4 https://github.com/onnx/models/tree/master/vision/object_detection_segmentation/yolov4
-            /*val top5 = predictTop5Labels(it, inputData, imageNetClassLabels)
+            val yhat = it.rawPredict(inputData)
+            val smallGrid = (yhat as List<Array<*>>)[2][0] as Array<Array<Array<FloatArray>>>
+            for (i in 0..12){
+                for (j in 0..12) {
+                    for (k in 0..2) {
+                        val row = smallGrid[i][j][k]
+                        if (row[4] > 0.002f)
+                            println(cocoCategories[row.copyOfRange(5, row.size).toList().argmax()])
+                        // TODO: it returns the first index, if probabilites are equal it will return the one value for vase or book every time
+                    }
+                }
+
+            }
+        // TODO: interpret the results of YOLOv4 https://github.com/onnx/models/tree/master/vision/object_detection_segmentation/yolov4
+        /*val top5 = predictTop5Labels(it, inputData, imageNetClassLabels)
 
 
             // TODO:
             // 1) interpret YOLOv4 numbers
             // 2) load YOLOv4 to ModelZoo
-            // 3) load coco.names to resources https://github.com/hunglc007/tensorflow-yolov4-tflite/blob/master/data/classes/coco.names
-            // 4) add YOLOv4 preprocessing for photos https://www.reddit.com/r/computervision/comments/mc6m87/how_do_you_add_a_class_to_coco_classes_in_yolo/
-            // 5) add YOLOv4 post-processing for photos https://www.reddit.com/r/computervision/comments/mc6m87/how_do_you_add_a_class_to_coco_classes_in_yolo/
+            // 4) add YOLOv4 preprocessing for photos https://github.com/onnx/models/tree/master/vision/object_detection_segmentation/yolov4#preprocessing-steps
+            // 5) add YOLOv4 post-processing for photos https://github.com/onnx/models/tree/master/vision/object_detection_segmentation/yolov4#preprocessing-steps
             // 6) draw image on batik or SWING with rectangles by coordinates https://github.com/onnx/models/tree/master/vision/object_detection_segmentation/yolov4
             // 7) write a tutorial for YOLOv4 + add info about transfer learning https://www.reddit.com/r/computervision/comments/mc6m87/how_do_you_add_a_class_to_coco_classes_in_yolo/
+            // 8) interpritation of yhat is here https://machinelearningmastery.com/how-to-perform-object-detection-with-yolov3-in-keras/
             println(top5.toString())*/
-        }
     }
+}
+}
+
+
+fun <T : Comparable<T>> Iterable<T>.argmax(): Int? {
+    return withIndex().maxByOrNull { it.value }?.index
 }
