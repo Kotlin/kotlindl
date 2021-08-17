0b3abe4a6dc5b4d872d16ca496ad7d5d7d76cd36
Masoud Kazemi <mkaze@users.noreply.github.com>
2021-08-17 18:43:30 +0430

    Add TimeStopping callback (#183)
    * Add TimeStopping callback
    
    * Refactor lenet model; Add TODO
    
    * Add copyright notice

--- examples/src/main/kotlin/examples/cnn/mnist/advanced/LeNetWithTimeStoppingCallback.kt
+++ examples/src/main/kotlin/examples/cnn/mnist/advanced/LeNetWithTimeStoppingCallback.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 JetBrains s.r.o. and Kotlin Deep Learning project contributors. All Rights Reserved.
+ * Copyright 2021 JetBrains s.r.o. and Kotlin Deep Learning project contributors. All Rights Reserved.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
  */
 
@@ -7,9 +7,7 @@ package examples.cnn.mnist.advanced
 
 import examples.cnn.models.buildLetNet5Classic
 import org.jetbrains.kotlinx.dl.api.core.activation.Activations
-import org.jetbrains.kotlinx.dl.api.core.callback.EarlyStopping
-import org.jetbrains.kotlinx.dl.api.core.callback.EarlyStoppingMode
-import org.jetbrains.kotlinx.dl.api.core.history.EpochTrainingEvent
+import org.jetbrains.kotlinx.dl.api.core.callback.TimeStopping
 import org.jetbrains.kotlinx.dl.api.core.loss.Losses
 import org.jetbrains.kotlinx.dl.api.core.metric.Metrics
 import org.jetbrains.kotlinx.dl.api.core.optimizer.Adam
@@ -41,29 +39,21 @@ private val lenet5Classic = buildLetNet5Classic(
  * It includes:
  * - dataset loading from S3
  * - callback definition
- * - model compilation with [EarlyStopping] callback
+ * - model compilation with [TimeStopping] callback
  * - model summary
  * - model training
  * - model evaluation
  */
-fun lenetWithEarlyStoppingCallback() {
+fun lenetWithTimeStoppingCallback() {
     val (train, test) = mnist()
 
     lenet5Classic.use {
-        val earlyStopping = EarlyStopping(
-            monitor = EpochTrainingEvent::valLossValue,
-            minDelta = 0.0,
-            patience = 2,
-            verbose = true,
-            mode = EarlyStoppingMode.AUTO,
-            baseline = 0.1,
-            restoreBestWeights = false
-        )
+        val timeStopping = TimeStopping(seconds = 30, verbose = true)
         it.compile(
             optimizer = Adam(clipGradient = ClipGradientByValue(0.1f)),
             loss = Losses.SOFT_MAX_CROSS_ENTROPY_WITH_LOGITS,
             metric = Metrics.ACCURACY,
-            callback = earlyStopping
+            callback = timeStopping,
         )
 
         it.summary()
@@ -76,5 +66,4 @@ fun lenetWithEarlyStoppingCallback() {
     }
 }
 
-/** */
-fun main(): Unit = lenetWithEarlyStoppingCallback()
+fun main(): Unit = lenetWithTimeStoppingCallback()
---
+++ examples/src/main/kotlin/examples/cnn/models/LeNet5Classic.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2021 JetBrains s.r.o. and Kotlin Deep Learning project contributors. All Rights Reserved.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
+
+package examples.cnn.models
+
+import org.jetbrains.kotlinx.dl.api.core.Sequential
+import org.jetbrains.kotlinx.dl.api.core.activation.Activations
+import org.jetbrains.kotlinx.dl.api.core.initializer.Constant
+import org.jetbrains.kotlinx.dl.api.core.initializer.GlorotNormal
+import org.jetbrains.kotlinx.dl.api.core.initializer.Zeros
+import org.jetbrains.kotlinx.dl.api.core.layer.convolutional.Conv2D
+import org.jetbrains.kotlinx.dl.api.core.layer.convolutional.ConvPadding
+import org.jetbrains.kotlinx.dl.api.core.layer.core.Dense
+import org.jetbrains.kotlinx.dl.api.core.layer.core.Input
+import org.jetbrains.kotlinx.dl.api.core.layer.pooling.AvgPool2D
+import org.jetbrains.kotlinx.dl.api.core.layer.reshaping.Flatten
+
+/**
+ * This is an CNN based on an implementation of LeNet-5 from classic paper.
+ *
+ * @see <a href="http://yann.lecun.com/exdb/publis/pdf/lecun-98.pdf">
+ *    Gradient-based learning applied to document recognition:[Yann LeCun, Léon Bottou, Yoshua Bengio, Patrick Haffner, 1998]</a>
+ */
+internal fun buildLetNet5Classic(
+    image_width: Long,
+    image_height: Long,
+    num_channels: Long,
+    num_classes: Int,
+    layers_activation: Activations,
+    classifier_activation: Activations,
+    random_seed: Long,
+): Sequential {
+    return Sequential.of(
+        Input(
+            image_width,
+            image_height,
+            num_channels,
+        ),
+        Conv2D(
+            filters = 6,
+            kernelSize = longArrayOf(5, 5),
+            strides = longArrayOf(1, 1, 1, 1),
+            activation = layers_activation,
+            kernelInitializer = GlorotNormal(random_seed),
+            biasInitializer = Zeros(),
+            padding = ConvPadding.SAME,
+        ),
+        AvgPool2D(
+            poolSize = intArrayOf(1, 2, 2, 1),
+            strides = intArrayOf(1, 2, 2, 1),
+            padding = ConvPadding.VALID,
+        ),
+        Conv2D(
+            filters = 16,
+            kernelSize = longArrayOf(5, 5),
+            strides = longArrayOf(1, 1, 1, 1),
+            activation = layers_activation,
+            kernelInitializer = GlorotNormal(random_seed),
+            biasInitializer = Zeros(),
+            padding = ConvPadding.SAME,
+        ),
+        AvgPool2D(
+            poolSize = intArrayOf(1, 2, 2, 1),
+            strides = intArrayOf(1, 2, 2, 1),
+            padding = ConvPadding.VALID,
+        ),
+        Flatten(),
+        Dense(
+            outputSize = 120,
+            activation = layers_activation,
+            kernelInitializer = GlorotNormal(random_seed),
+            biasInitializer = Constant(0.1f),
+        ),
+        Dense(
+            outputSize = 84,
+            activation = Activations.Tanh,
+            kernelInitializer = GlorotNormal(random_seed),
+            biasInitializer = Constant(0.1f),
+        ),
+        Dense(
+            outputSize = num_classes,
+            activation = classifier_activation,
+            kernelInitializer = GlorotNormal(random_seed),
+            biasInitializer = Constant(0.1f),
+        )
+    )
+}
