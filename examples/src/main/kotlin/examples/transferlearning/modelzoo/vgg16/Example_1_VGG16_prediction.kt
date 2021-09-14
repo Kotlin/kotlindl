82f70b14f941b610d0c0a522168b6bc6823e0193
Alexey Zinoviev <zaleslaw.sin@gmail.com>
2021-09-14 14:50:05 +0300

    Added a missed example

--- examples/src/main/kotlin/examples/transferlearning/modelzoo/vgg16/Example_1_VGG16_prediction.kt
+++ examples/src/main/kotlin/examples/transferlearning/modelzoo/vgg16/Example_1_VGG16_prediction.kt
@@ -6,11 +6,84 @@
 package examples.transferlearning.modelzoo.vgg16
 
 
+import org.jetbrains.kotlinx.dl.api.core.Sequential
+import org.jetbrains.kotlinx.dl.api.core.loss.Losses
+import org.jetbrains.kotlinx.dl.api.core.metric.Metrics
+import org.jetbrains.kotlinx.dl.api.core.optimizer.Adam
+import org.jetbrains.kotlinx.dl.api.inference.keras.loadWeights
+import org.jetbrains.kotlinx.dl.api.inference.keras.loaders.TFModelHub
+import org.jetbrains.kotlinx.dl.api.inference.keras.loaders.TFModels
+import org.jetbrains.kotlinx.dl.api.inference.keras.loaders.predictTop5ImageNetLabels
+import org.jetbrains.kotlinx.dl.dataset.image.ColorOrder
+import org.jetbrains.kotlinx.dl.dataset.preprocessor.ImageShape
+import org.jetbrains.kotlinx.dl.dataset.preprocessor.Preprocessing
+import org.jetbrains.kotlinx.dl.dataset.preprocessor.image.load
+import org.jetbrains.kotlinx.dl.dataset.preprocessor.preprocess
+import org.jetbrains.kotlinx.dl.dataset.preprocessor.transformImage
 import java.io.File
 import java.net.URISyntaxException
 import java.net.URL
 
+/**
+ * This examples demonstrates the inference concept on VGG'16 model:
+ * - Model configuration, model weights and labels are obtained from [TFModelHub].
+ * - Weights are loaded from .h5 file, configuration is loaded from .json file.
+ * - Model predicts on a few images located in resources.
+ * - Special preprocessing (used in VGG'16 during training on ImageNet dataset) is applied to images before prediction.
+ * - No additional training.
+ * - No new layers are added.
+ *
+ * @see <a href="https://arxiv.org/abs/1409.1556">
+ *     Very Deep Convolutional Networks for Large-Scale Image Recognition (ICLR 2015).</a>
+ * @see <a href="https://keras.io/api/applications/vgg/#vgg16-function">
+ *    Detailed description of VGG'16 model and an approach to build it in Keras.</a>
+ */
+fun vgg16prediction() {
+    val modelHub = TFModelHub(cacheDirectory = File("cache/pretrainedModels"))
+    val modelType = TFModels.CV.VGG16
+    val model = modelHub.loadModel(modelType)
 
+    val imageNetClassLabels = modelHub.loadClassLabels()
+
+    model.use {
+        it.compile(
+            optimizer = Adam(),
+            loss = Losses.MAE,
+            metric = Metrics.ACCURACY
+        )
+        println(it.kGraph)
+
+        it.summary()
+
+        val hdfFile = modelHub.loadWeights(modelType)
+
+        it.loadWeights(hdfFile)
+
+        for (i in 1..8) {
+            val preprocessing: Preprocessing = preprocess {
+                transformImage {
+                    load {
+                        pathToData = getFileFromResource("datasets/vgg/image$i.jpg")
+                        imageShape = ImageShape(224, 224, 3)
+                        colorMode = ColorOrder.BGR
+                    }
+                }
+            }
+
+            val inputData = modelType.preprocessInput(preprocessing().first, model.inputDimensions)
+
+            val res = it.predict(inputData, "Activation_predictions")
+            println("Predicted object for image$i.jpg is ${imageNetClassLabels[res]}")
+
+            val top5 = predictTop5ImageNetLabels(it, inputData, imageNetClassLabels)
+
+            println(top5.toString())
+        }
+    }
+}
+
+/** */
+fun main(): Unit = vgg16prediction()
 
 /** Converts resource string path to the file. */
 @Throws(URISyntaxException::class)
