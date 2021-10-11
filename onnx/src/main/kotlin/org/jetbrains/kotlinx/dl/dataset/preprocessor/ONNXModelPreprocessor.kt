df2e35d95f67f7db8d16430903a8bd815b332a47
Julia Beliaeva <juliabeliaeva@users.noreply.github.com>
2021-10-11 13:03:54 +0300

    Remove CustomPreprocessor (#257)
    Since the refactoring in 540bf0316d38d6216ef99b8afd8403cdd72e8889 any number of preprocessors of any kind could be used, so there is no need to have a special "custom" one.

--- onnx/src/main/kotlin/org/jetbrains/kotlinx/dl/dataset/preprocessor/ONNXModelPreprocessor.kt
+++ onnx/src/main/kotlin/org/jetbrains/kotlinx/dl/dataset/preprocessor/ONNXModelPreprocessor.kt
@@ -23,5 +23,5 @@ public class ONNXModelPreprocessor(public var onnxModel: OnnxInferenceModel?, pu
 
 /** Image DSL Preprocessing extension.*/
 public fun TensorPreprocessing.onnx(block: ONNXModelPreprocessor.() -> Unit) {
-    addOperation(CustomPreprocessor(ONNXModelPreprocessor(null).apply(block)))
+    addOperation(ONNXModelPreprocessor(null).apply(block))
 }
