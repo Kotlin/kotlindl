36290cf4196941fdbba2557229066e5de30958fd
xiaoshen <xiaoshen@eridani.club>
2021-12-16 00:59:58 -0600

    Dsl Builder

---
+++ api/src/test/kotlin/org/jetbrains/kotlinx/dl/api/core/dsl/GraphTrainableModelDslTest.kt
@@ -0,0 +1,54 @@
+package org.jetbrains.kotlinx.dl.api.core.dsl
+
+import org.jetbrains.kotlinx.dl.api.core.Functional
+import org.jetbrains.kotlinx.dl.api.core.layer.convolutional.Conv1D
+import org.jetbrains.kotlinx.dl.api.core.layer.core.Input
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class GraphTrainableModelDslTest {
+    @Test
+    fun buildSequential() {
+        assertDoesNotThrow {
+            sequential {
+                model {
+                    name = "Test Sequential"
+                }
+
+                layers {
+                    +Input(128, 128)
+                    +Conv1D()
+                }
+            }
+        }
+
+    }
+
+    @Test
+    fun buildFunctional() {
+        assertDoesNotThrow {
+            functional {
+                model {
+                    name = "Test Functional"
+                }
+
+                layers {
+                    +Input(128, 128)
+                    +Conv1D()
+                }
+            }
+        }
+    }
+
+    @Test
+    fun genericBuilder() {
+        assertDoesNotThrow {
+            ::Functional {
+                layers {
+                    +Input(128, 128, name = "First Layer")
+                    +Conv1D(name = "Second Layer")
+                }
+            }
+        }
+    }
+}
--- api/src/test/kotlin/org/jetbrains/kotlinx/dl/api/inference/keras/InputLayerPersistenceTest.kt
+++ api/src/test/kotlin/org/jetbrains/kotlinx/dl/api/inference/keras/InputLayerPersistenceTest.kt
@@ -2,6 +2,8 @@ package org.jetbrains.kotlinx.dl.api.inference.keras
 
 import org.jetbrains.kotlinx.dl.api.core.Functional
 import org.jetbrains.kotlinx.dl.api.core.Sequential
+import org.jetbrains.kotlinx.dl.api.core.dsl.functional
+import org.jetbrains.kotlinx.dl.api.core.dsl.sequential
 import org.jetbrains.kotlinx.dl.api.core.layer.core.Input
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -36,6 +38,70 @@ class InputLayerPersistenceTest {
         testFunctionalModel(Functional.of(Input(128, 128)))
         testFunctionalModel(Functional.of(Input(128, 128, 3)))
         testFunctionalModel(Functional.of(Input(10, 10, 10, 10)))
+    }
+
+    @Test
+    fun dslBuilderSequential() {
+        testSequentialModel(
+            sequential {
+                layers {
+                    +Input(4)
+                }
+            }
+        )
+        testSequentialModel(
+            sequential {
+                layers {
+                    +Input(128, 128)
+                }
+            }
+        )
+        testSequentialModel(
+            sequential {
+                layers {
+                    +Input(128, 128, 3)
+                }
+            }
+        )
+        testSequentialModel(
+            sequential {
+                layers {
+                    +Input(10, 10, 10, 10)
+                }
+            }
+        )
+    }
+
+    @Test
+    fun dslBuilderFunctional() {
+        testFunctionalModel(
+            functional {
+                layers {
+                    +Input(4)
+                }
+            }
+        )
+        testFunctionalModel(
+            functional {
+                layers {
+                    +Input(128, 128)
+                }
+            }
+        )
+        testFunctionalModel(
+            functional {
+                layers {
+                    +Input(128, 128, 3)
+                }
+            }
+        )
+        testFunctionalModel(
+            functional {
+                layers {
+                    +Input(10, 10, 10, 10)
+                }
+            }
+        )
     }
 
     private fun testSequentialModel(originalModel: Sequential) {
