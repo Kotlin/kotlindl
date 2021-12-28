12ebd82a6b5b69cdbc2f9a66a1d3c07371cbd578
Julia Beliaeva <juliabeliaeva@users.noreply.github.com>
2021-12-28 21:07:09 +0300

    Fix loadModelLayersFromConfiguration recursively calling itself (#319)

--- api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/Functional.kt
+++ api/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/Functional.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 JetBrains s.r.o. and Kotlin Deep Learning project contributors. All Rights Reserved.
+ * Copyright 2020-2021 JetBrains s.r.o. and Kotlin Deep Learning project contributors. All Rights Reserved.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
  */
 
@@ -173,7 +173,8 @@ public class Functional(vararg layers: Layer) : GraphTrainableModel(*layers) {
         ): MutableList<Layer> {
             require(configuration.isFile) { "${configuration.absolutePath} is not a file. Should be a .json file with configuration." }
 
-            return loadModelLayersFromConfiguration(configuration, inputShape)
+            val functionalConfig = loadSerializedModel(configuration)
+            return loadFunctionalModelLayers(functionalConfig, inputShape)
         }
 
         /**
