9ce1765f9eb472377bbbbb31052990c3e61c442a
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2022-05-18 01:53:18 +0300

    Rename test method according to the project code style

--- api/src/test/kotlin/org/jetbrains/kotlinx/dl/api/inference/keras/ModelLoaderTest.kt
+++ api/src/test/kotlin/org/jetbrains/kotlinx/dl/api/inference/keras/ModelLoaderTest.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2022 JetBrains s.r.o. and Kotlin Deep Learning project contributors. All Rights Reserved.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
+
 package org.jetbrains.kotlinx.dl.api.inference.keras
 
 import org.jetbrains.kotlinx.dl.api.core.initializer.*
@@ -11,7 +16,7 @@ private const val INITIALIZER_PATH = "src/test/resources/inference/keras/ModelLo
 
 class ModelLoaderTest {
     @Test
-    fun `load initializers from file`() {
+    fun loadInitializersFromFile() {
         val initializerFile = File(INITIALIZER_PATH)
 
         val modelConfiguration = loadSequentialModelConfiguration(initializerFile)
