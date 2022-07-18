c6685c1d67ebf91554926e50486fcc8a8e4dfe37
ermolenkodev <nikita.ermolenko@jetbrains.com>
2022-07-18 13:51:33 +0300

    Fix loading of jpeg files not supported by standard java ImageIO (#384) (#404)
    * Add TwelveMonkeys ImageIO plugin
    
    * Add IO test with images from COCO 2017 test set, which failed to load using standard java ImageIO

---
+++ examples/src/test/kotlin/examples/io/IOTestSuite.kt
@@ -0,0 +1,31 @@
+package examples.io
+
+import examples.transferlearning.getFileFromResource
+import org.jetbrains.kotlinx.dl.dataset.image.ImageConverter
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import javax.imageio.ImageIO
+import javax.imageio.ImageReader
+
+
+class IOTestSuite {
+    private val unsupportedJpegsFolder = getFileFromResource("jpeg_unsupported")
+
+    @Test
+    fun testJvmUnsupportedJpegLoading() {
+        val imageFiles = unsupportedJpegsFolder.walk().filter { it.isFile }
+
+        for (imageFile in imageFiles) {
+            assertDoesNotThrow("Failed to load image file $imageFile") {
+                imageFile.inputStream().use { inputStream -> ImageConverter.toBufferedImage(inputStream) }
+            }
+        }
+    }
+
+    @Test
+    fun testImageioJpegPluginIsUsed() {
+        val readers: Iterator<ImageReader> = ImageIO.getImageReadersByFormatName("JPEG")
+        val defaultReader = readers.next()
+        assert("com.twelvemonkeys.imageio.plugins.jpeg.JPEGImageReader" in defaultReader.toString()) { "Default jpeg reader should be com.twelvemonkeys.imageio.plugins.jpeg.JPEGImageReader" }
+    }
+}
