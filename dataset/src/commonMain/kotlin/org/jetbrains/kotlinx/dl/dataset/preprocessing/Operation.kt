ac72b5d0ab3c015601fd2e8ae1caa6a7e71146be
Nikita Ermolenko <nikita.ermolenko@jetbrains.com>
2022-08-22 17:28:35 +0300

    Add documentation for the Operation interface (#416)

--- dataset/src/commonMain/kotlin/org/jetbrains/kotlinx/dl/dataset/preprocessing/Operation.kt
+++ dataset/src/commonMain/kotlin/org/jetbrains/kotlinx/dl/dataset/preprocessing/Operation.kt
@@ -8,7 +8,16 @@ import org.jetbrains.kotlinx.dl.api.core.shape.TensorShape
  * @param O is a type of an output.
  */
 public interface Operation<I, O> {
+    /**
+     * Performs preprocessing operation on the input.
+     * @param input is an input to the operation of type [I].
+     * @return an output of the operation of type [O].
+     */
     public fun apply(input: I): O
+    /**
+     * Returns shape of the output of the operation having input of shape [inputShape].
+     * @param inputShape is a shape of the input.
+     */
     public fun getOutputShape(inputShape: TensorShape): TensorShape
 }
 
@@ -17,7 +26,5 @@ public interface Operation<I, O> {
  */
 public class Identity<I> : Operation<I, I> {
     override fun apply(input: I): I = input
-    override fun getOutputShape(inputShape: TensorShape): TensorShape {
-        return inputShape
-    }
+    override fun getOutputShape(inputShape: TensorShape): TensorShape = inputShape
 }
