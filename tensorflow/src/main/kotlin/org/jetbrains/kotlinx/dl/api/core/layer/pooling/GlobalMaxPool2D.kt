e1dca8df9cf3256edf1f71f79bcf2515207971c9
Dosier <stanvdbend@gmail.com>
2022-09-29 08:23:32 +0200

    Added shape docs for GlobalMaxPool2D layer from Keras (#211)
    https://keras.io/api/layers/pooling_layers/global_max_pooling2d/

--- tensorflow/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/layer/pooling/GlobalMaxPool2D.kt
+++ tensorflow/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/layer/pooling/GlobalMaxPool2D.kt
@@ -14,6 +14,10 @@ import org.tensorflow.op.Ops
  *
  * Downsamples the input by taking the maximum value over spatial dimensions.
  *
+ * __Input shape:__  4D tensor with shape `(batch_size, rows, cols, channels)`.
+ *
+ * __Output shape:__ 2D tensor with shape `(batch_size, channels)`.
+ *
  * @since 0.3
  */
 public class GlobalMaxPool2D(
