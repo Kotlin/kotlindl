d4987ead2af186cf1ab11a18a563da51c97a8cb4
Julia Beliaeva <juliabeliaeva@users.noreply.github.com>
2023-01-02 15:31:39 +0100

    Fix HardShrink activation (#505)
    Build a graph of operations in the apply function instead of incorrectly trying to perform these operations.

--- tensorflow/src/test/kotlin/org/jetbrains/kotlinx/dl/api/core/activation/HardShrinkActivationTest.kt
+++ tensorflow/src/test/kotlin/org/jetbrains/kotlinx/dl/api/core/activation/HardShrinkActivationTest.kt
@@ -4,10 +4,23 @@ import org.junit.jupiter.api.Test
 
 class HardShrinkActivationTest : ActivationTest() {
     @Test
-    fun apply() {
-        val input = floatArrayOf(1f, 0f, 1f)
-        val expected = floatArrayOf(1f, 0f, 1f)
-        val actual = floatArrayOf(0f, 0f, 0f)
-        assertActivationFunction(HardShrinkActivation(-0.5f, 0.5f), input, actual, expected)
+    fun mask() {
+        val input = floatArrayOf(1f, 0f, -1f)
+        val expected = floatArrayOf(1f, 0f, -1f)
+        assertActivationFunction(HardShrinkActivation(-0.5f, 0.5f), input, expected)
+    }
+
+    @Test
+    fun setToZero() {
+        val input = floatArrayOf(0.239f, 0.01f, -0.239f)
+        val expected = floatArrayOf(0f, 0f, 0f)
+        assertActivationFunction(HardShrinkActivation(-0.5f, 0.5f), input, expected)
+    }
+
+    @Test
+    fun mixed() {
+        val input = floatArrayOf(0.239f, -5f, -10f, 0.3f, -0.5f, 239f, 0.7f, -100f, -0.4f)
+        val expected = floatArrayOf(0f, -5f, -10f, 0f, -0.5f, 239f, 0f, -100f, -0f)
+        assertActivationFunction(HardShrinkActivation(-0.4f, 0.7f), input, expected)
     }
 }
