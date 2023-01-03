f586b7406a1d9762c94249fb3bd44d62f0b1478d
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2023-01-03 17:38:44 +0100

    Remove unused constants

--- tensorflow/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/util/tensorNames.kt
+++ tensorflow/src/main/kotlin/org/jetbrains/kotlinx/dl/api/core/util/tensorNames.kt
@@ -9,12 +9,8 @@ internal const val TRAINING_LOSS = "default_training_loss"
 
 internal const val OUTPUT_NAME = "default_output"
 
-internal const val CLASSIFIER_OUTPUT_NAME = "classifier_output"
-
 internal const val OUTPUT_ARG_MAX = "ArgMax"
 
 internal const val DATA_PLACEHOLDER = "default_data_placeholder"
-
-internal const val LABEL_PLACEHOLDER = "default_label_placeholder"
 
 internal const val PLACEHOLDER = "Placeholder"
